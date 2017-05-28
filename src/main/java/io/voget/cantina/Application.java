c504a0686d1bab204bd10343fb14de730f0579a0
Matt Voget <matt.voget@gmail.com>
2017-05-28 17:26:19 -0600

    Updated sarlacc client and protected song creation and removal

--- src/main/java/io/voget/cantina/Application.java
+++ src/main/java/io/voget/cantina/Application.java
@@ -1,48 +1,41 @@
 package io.voget.cantina;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 import com.mattvoget.sarlacc.client.SarlaccUserService;
+import com.mattvoget.sarlacc.client.SarlaccUserServiceImpl;
+import com.mattvoget.sarlacc.models.App;
 
 @SpringBootApplication
 public class Application {
 	
-	public static void main(String[] args) throws FileNotFoundException, IOException {		
+	public static void main(String[] args) {		
         SpringApplication.run(Application.class, args);
     }
-	
-	@Bean
-	@Autowired
-	public GridFsTemplate gridFsTemplate(MongoDbFactory mongoDbFactory, MappingMongoConverter mappingMongoConverter) throws Exception {
-		return new GridFsTemplate(mongoDbFactory, mappingMongoConverter);
-	}
-	
+		
     // Sarlacc Client Configuration ===================================
-    @Value("${auth.url.token}")
-    private String authUrlToken;
+    @Value("${sarlacc.url}")
+    private String sarlaccUrl;
 
-    @Value("${auth.url.user}")
-    private String authUrlUser;
+    @Value("${sarlacc.client.id}")
+    private String sarlaccClientId;
 
-    @Value("${auth.client.id}")
-    private String authClientId;
-
-    @Value("${auth.client.password}")
-    private String authClientPassword;
+    @Value("${sarlacc.client.password}")
+    private String sarlaccClientPassword;
 
     @Bean
     public SarlaccUserService sarlaccUserService(){
-        return new SarlaccUserService(authUrlToken,authUrlUser,authClientId,authClientPassword);
+        return new SarlaccUserServiceImpl(sarlaccUrl,sarlaccClientId,sarlaccClientPassword);
+    }
+    
+    @Bean
+    public App app() {
+    	App app = new App();
+    	app.setName("the-cantina");
+    	return app;
     }
 
 }
--- src/main/java/io/voget/cantina/controllers/SongController.java
+++ src/main/java/io/voget/cantina/controllers/SongController.java
@@ -1,5 +1,7 @@
 package io.voget.cantina.controllers;
 
+import static com.mattvoget.sarlacc.client.SarlaccUserService.TOKEN_NAME;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -8,12 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.mattvoget.sarlacc.client.SarlaccUserException;
+import com.mattvoget.sarlacc.client.SarlaccUserService;
+import com.mattvoget.sarlacc.models.App;
 
 import io.voget.cantina.models.Song;
 import io.voget.cantina.services.SongService;
@@ -22,7 +29,11 @@ import io.voget.cantina.services.SongService;
 @RequestMapping(value="songs", produces= MediaType.APPLICATION_JSON_VALUE)
 public class SongController {
 		
+	@Autowired SarlaccUserService userService;
+	@Autowired App app;
+	
 	@Autowired SongService songSvc;
+	
 	
     @RequestMapping(value="/", method= RequestMethod.GET)
     @ResponseBody
@@ -39,16 +50,28 @@ public class SongController {
     @RequestMapping(value="/{songId}", method= RequestMethod.DELETE)
     @ResponseBody
     @ResponseStatus(code=HttpStatus.OK)
-    public void deleteSong(@PathVariable String songId) {  
+    public void deleteSong(@RequestHeader(value=TOKEN_NAME) String accessToken, @PathVariable String songId) {  
+    	
+    	if (!userService.isUserAdminForApp(accessToken, app)) {
+			throw new SarlaccUserException("User does not have permissions to access this API!");
+		}
+    	
     	songSvc.deleteSong(songId);
     }
 
 	@RequestMapping(value="/", method=RequestMethod.POST, consumes = "multipart/form-data")
 	@ResponseBody
 	public Song createNewSong(
+			@RequestHeader(value=TOKEN_NAME) String accessToken,
 			@RequestParam("name") String songName,
 			@RequestParam("filename") String songFilename,
 			@RequestParam("song") MultipartFile songData) throws IOException {
+		
+		if (!userService.isUserAdminForApp(accessToken, app)) {
+			throw new SarlaccUserException("User does not have permissions to access this API!");
+		}
+		
+		
 		return songSvc.createNewSong(songName, songFilename, songData.getBytes());
 	}
 	
